b9388b25b1d84da7603cabe3a10debd3fd16f6e4
timur <timurka.zavyalov@mail.ru>
2023-09-24 14:39:55 +0300

    fix: optimization code

--- all_tasks.py
+++ all_tasks.py
@@ -1,9 +1,10 @@
+import copy
 import os
 import glob
 import pprint
 import pandas as pd
 from pandas import DataFrame
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from datetime import datetime, timedelta
 
 
@@ -121,6 +122,138 @@ class ConcatenationWords:
         return concat_words
 
 
+#  Задание 5. Другое решение
+class ConcatenationWordsTwo:
+
+    @staticmethod
+    def convert_words_to_dicts(words: List[str]) -> dict:
+        """
+
+        :param words:
+        :return:
+        """
+        dict_nested_letters: dict = {}
+        for word in words:
+            current_dict: dict = dict_nested_letters
+            list_same_and_words: list = []
+            for letter in word:
+                if letter not in current_dict:
+                    if list_same_and_words and all(list_same_and_words):
+                        current_dict[''] = {}
+                    current_dict[letter] = {}
+                    list_same_and_words.append(False)
+                current_dict = current_dict[letter]
+                list_same_and_words.append(True)
+        return dict_nested_letters
+
+    @staticmethod
+    def get_list_letters_from_dict(nested_dict: dict) -> List[List[str]]:
+        """
+
+        :param nested_dict:
+        :return:
+        """
+        all_letters: List[List[str]] = []
+        letters: List[str] = []
+        while nested_dict:
+            letter: str = next(iter(nested_dict.keys()))
+            if letter:
+                letters.append(letter)
+                nested_dict = nested_dict[letter]
+            else:
+                nested_dict.pop(letter)
+                all_letters.append(letters.copy())
+        all_letters.append(letters)
+        return all_letters
+
+    @staticmethod
+    def merge_two_dicts(letters: list, current_dict: dict) -> dict:
+        """
+
+        :param letters:
+        :param current_dict:
+        :return:
+        """
+        for letter in letters:
+            if letter not in current_dict:
+                current_dict[letter] = {}
+            current_dict = current_dict[letter]
+        return current_dict
+
+    def merge_list_to_dict(self, letters: list, nested_dict: dict) -> dict:
+        """
+
+        :param letters:
+        :param nested_dict:
+        :return:
+        """
+        result_dict: dict = {}
+        current_dict: dict = result_dict
+
+        current_dict = self.merge_two_dicts(letters, current_dict)
+        for letters in self.get_list_letters_from_dict(nested_dict):
+            current_dict = self.merge_two_dicts(letters, current_dict)
+
+        return result_dict
+
+    def remove_keys_in_order(self, dict_nested_letters: dict, keys: list) -> dict:
+        """
+
+        :param dict_nested_letters:
+        :param keys:
+        :return:
+        """
+        if not keys:
+            return dict_nested_letters
+        key: str = keys.pop(0)
+        if key in dict_nested_letters:
+            dict_nested_letters[key] = self.remove_keys_in_order(dict_nested_letters[key], keys)
+            if not dict_nested_letters[key]:
+                del dict_nested_letters[key]
+        return dict_nested_letters
+    
+    @staticmethod
+    def find_same_letters(input_word: str, dict_nested_letters: dict) -> Tuple[dict, list]:
+        """
+
+        :param input_word:
+        :param dict_nested_letters:
+        :return:
+        """
+        current_dict: dict = dict_nested_letters
+        same_letters: list = []
+        for letter in input_word:
+            if letter in current_dict:
+                current_dict = current_dict[letter]
+                same_letters.append(letter)
+            else:
+                current_dict = dict_nested_letters
+                same_letters = [letter] if letter in dict_nested_letters else []
+        return current_dict, same_letters
+
+    def main(self, input_word: str, results: list, dict_nested_letters: dict):
+        """
+
+        :param input_word:
+        :param results:
+        :param dict_nested_letters:
+        :return:
+        """
+        dict_nested_letters: dict = copy.deepcopy(dict_nested_letters)
+        current_dict, same_letters = self.find_same_letters(input_word, dict_nested_letters)
+        if len(same_letters) <= 1:
+            return None
+        list_same_letters: list = self.get_list_letters_from_dict(current_dict)
+        for letters in list_same_letters:
+            new_word: str = input_word + "".join(letters)
+            result_dict: List[List[str]] = self.get_list_letters_from_dict(self.merge_list_to_dict(same_letters, current_dict))
+            for list_letters in result_dict:
+                self.remove_keys_in_order(dict_nested_letters, list_letters)
+            self.main(input_word, results, dict_nested_letters)
+            if new_word != input_word:
+                results.append(new_word)
+
+
 if __name__ == "__main__":
     print("Задание 1")
     print(save_unique_data())  # Задание 1
@@ -133,4 +266,15 @@ if __name__ == "__main__":
     print("\nЗадание 5")
     concatenation_words: ConcatenationWords = ConcatenationWords()
     list_words: List[str] = concatenation_words.read_file("task5.txt")
-    print(concatenation_words.concatenate_words(list_words))  # Задание 5
+
+    # print(concatenation_words.concatenate_words(list_words))  # Задание 5
+
+    print("\nЗадание 5. Другое решение")
+    concatenation_words_two: ConcatenationWordsTwo = ConcatenationWordsTwo()
+    dict_nested_letters_ = concatenation_words_two.convert_words_to_dicts(list_words)
+
+    for _ in range(3):
+        results_concat_words: List[str] = []
+        custom_word: str = input("Введите любое слово из файла task5.txt: \n<<< ")
+        concatenation_words_two.main(custom_word, results_concat_words, dict_nested_letters_)  # Задание 5
+        print(results_concat_words)
